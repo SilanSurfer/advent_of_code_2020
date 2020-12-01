e5a9429e8504e396a089a993164d1aa20ee4afd0
Adam Szwarc <ext-lux-as1@ad.datcon.co.uk>
2020-12-02 00:08:58 +0100

    refactor: Function for parsing input is more functional
    Changed implementation to be more functional.
    Corrected code formating.

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,9 +1,8 @@
 use std::str::FromStr;
 
 pub fn get_numbers_from_input(input: &String) -> Vec<i64> {
-    let mut numbers: Vec<i64> = Vec::new();
-    for line in input.lines() {
-        numbers.push(i64::from_str(line).unwrap());
-    }
-    numbers
+    input
+        .lines()
+        .map(|line| i64::from_str(line).unwrap())
+        .collect::<Vec<i64>>()
 }
