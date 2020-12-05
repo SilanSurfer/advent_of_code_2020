bd6cbc68ede94cbda78c27889d1c9c425c03558c
SilanSurfer <szwarc.adam@gmail.com>
2020-12-05 21:10:25 +0100

    refactor: Added error handling with anyhow for day 5
    Errors are handled with crate anyhow.
    Fixed clippy warnings.

--- day_05/src/solutions.rs
+++ day_05/src/solutions.rs
@@ -1,22 +1,13 @@
-use std::fmt;
 use std::str::FromStr;
-
-#[derive(Debug)]
-struct BoardingSeatError(String);
-
-impl fmt::Display for BoardingSeatError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
+use anyhow::{Result, anyhow};
 
 #[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
 struct BoardingSeat(u64);
 
 impl FromStr for BoardingSeat {
-    type Err = BoardingSeatError;
+    type Err = anyhow::Error;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+    fn from_str(s: &str) -> Result<Self> {
         let mut row_no: Vec<u32> = (0..128).collect();
         let mut col_no: Vec<u32> = (0..8).collect();
         let mut split_point = row_no.len();
@@ -26,7 +17,9 @@ impl FromStr for BoardingSeat {
             match letter {
                 'F' => row_no = left.to_vec(),
                 'B' => row_no = right.to_vec(),
-                _ => {}
+                _ => return Err(
+                    anyhow!("from_str -> letter should be F or B, found {}",
+                    letter)),
             }
         }
 
@@ -37,16 +30,26 @@ impl FromStr for BoardingSeat {
             match letter {
                 'R' => col_no = right.to_vec(),
                 'L' => col_no = left.to_vec(),
-                _ => {}
+                _ => return Err(
+                    anyhow!("from_str -> letter should be R or L, found {}",
+                    letter)),
             }
         }
-        let row = row_no.pop().unwrap();
-        let col = col_no.pop().unwrap();
+        let row = row_no
+                    .pop()
+                    .ok_or_else(|| {
+                        anyhow!("Pop failed! row_no vector is empty?")
+                    })?;
+        let col = col_no
+                    .pop()
+                    .ok_or_else(|| {
+                        anyhow!("Pop failed! col_no vector is empty?")
+                    })?;
         Ok(BoardingSeat(row as u64 * 8 + col as u64))
     }
 }
 
-fn get_sorted_seat_ids(lines: &Vec<&str>) -> Vec<BoardingSeat> {
+fn get_sorted_seat_ids(lines: &[&str]) -> Vec<BoardingSeat> {
     let mut ids: Vec<BoardingSeat> = lines
         .iter()
         .map(|line| match BoardingSeat::from_str(line) {
@@ -58,14 +61,14 @@ fn get_sorted_seat_ids(lines: &Vec<&str>) -> Vec<BoardingSeat> {
     ids
 }
 
-pub fn get_highest_seat_id(lines: &Vec<&str>) -> u64 {
+pub fn get_highest_seat_id(lines: &[&str]) -> u64 {
     get_sorted_seat_ids(lines)
         .iter()
         .max()
         .expect("Couldn't get max value").0
 }
 
-pub fn get_your_seat_id(lines: &Vec<&str>) -> u64 {
+pub fn get_your_seat_id(lines: &[&str]) -> u64 {
     let seats = get_sorted_seat_ids(lines);
     seats
         .iter()
