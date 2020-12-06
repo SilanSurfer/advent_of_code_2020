76388993fb6ba09cd176b46f9c29782ff6353ab4
SilanSurfer <szwarc.adam@gmail.com>
2020-12-06 13:34:08 +0100

    feat: Solution for part 2 of day 6

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -8,5 +8,7 @@ fn main() {
         .unwrap_or_else(|_| panic!("Couldn't open file {}", file_path));
     let input_vec = utils::get_lines_from_input_at_empty_line_split(&contents);
     println!("Sum of unique yes answers: {}",
-        solutions::get_sum_of_questions_with_yes_answer(&input_vec))
+        solutions::get_sum_of_questions_with_yes_answer(&input_vec));
+    println!("Sum of question with everyone in group yes answered: {}",
+        solutions::get_sum_of_questions_with_everyone_yes_answer(&input_vec));
 }
--- day_06/src/solutions.rs
+++ day_06/src/solutions.rs
@@ -1,5 +1,7 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
+/// For each group, count the number of questions to which anyone answered "yes".
+/// What is the sum of those counts?
 pub fn get_sum_of_questions_with_yes_answer(input: &[&str]) -> u64 {
     let mut result: u64 = 0;
     for group in input.iter() {
@@ -9,6 +11,28 @@ pub fn get_sum_of_questions_with_yes_answer(input: &[&str]) -> u64 {
         }
         // group.lines().map(|line| line.to_str()).chars().try_for_each(|c| set.insert(c));
         result += set.iter().count() as u64;
+    }
+    result
+}
+
+
+/// For each group, count the number of questions to which everyone answered "yes".
+/// What is the sum of those counts?
+pub fn get_sum_of_questions_with_everyone_yes_answer(input: &[&str]) -> u64 {
+    let mut result: u64 = 0;
+    for group in input.iter() {
+        let mem_no: u32 = group.lines().count() as u32;
+        let mut answers_map: HashMap<char, u32> = HashMap::new();
+        for line in group.lines() {
+            line
+                .chars()
+                .for_each(|c| {
+                    if let Some(val) = answers_map.insert(c, 1) {
+                        answers_map.insert(c, val + 1);
+                    }
+                });
+        }
+        result += answers_map.iter().filter(|(_, val)| **val == mem_no).count() as u64
     }
     result
 }
