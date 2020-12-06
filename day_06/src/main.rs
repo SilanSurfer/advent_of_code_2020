19974a9d3a01c6b7b3ffe8f0993628d18b3b07c0
SilanSurfer <szwarc.adam@gmail.com>
2020-12-06 13:38:29 +0100

    style: Formatted code in day_06 package

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -7,8 +7,12 @@ fn main() {
     let contents = fs::read_to_string(file_path)
         .unwrap_or_else(|_| panic!("Couldn't open file {}", file_path));
     let input_vec = utils::get_lines_from_input_at_empty_line_split(&contents);
-    println!("Sum of unique yes answers: {}",
-        solutions::get_sum_of_questions_with_yes_answer(&input_vec));
-    println!("Sum of question with everyone in group yes answered: {}",
-        solutions::get_sum_of_questions_with_everyone_yes_answer(&input_vec));
+    println!(
+        "Sum of unique yes answers: {}",
+        solutions::get_sum_of_questions_with_yes_answer(&input_vec)
+    );
+    println!(
+        "Sum of question with everyone in group yes answered: {}",
+        solutions::get_sum_of_questions_with_everyone_yes_answer(&input_vec)
+    );
 }
--- day_06/src/solutions.rs
+++ day_06/src/solutions.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, HashMap};
+use std::collections::{HashMap, HashSet};
 
 /// For each group, count the number of questions to which anyone answered "yes".
 /// What is the sum of those counts?
@@ -7,14 +7,15 @@ pub fn get_sum_of_questions_with_yes_answer(input: &[&str]) -> u64 {
     for group in input.iter() {
         let mut set: HashSet<char> = HashSet::new();
         for line in group.lines() {
-            line.chars().for_each(|c| { set.insert(c); } );
+            line.chars().for_each(|c| {
+                set.insert(c);
+            });
         }
         // group.lines().map(|line| line.to_str()).chars().try_for_each(|c| set.insert(c));
         result += set.iter().count() as u64;
     }
     result
 }
-
 
 /// For each group, count the number of questions to which everyone answered "yes".
 /// What is the sum of those counts?
@@ -24,15 +25,16 @@ pub fn get_sum_of_questions_with_everyone_yes_answer(input: &[&str]) -> u64 {
         let mem_no: u32 = group.lines().count() as u32;
         let mut answers_map: HashMap<char, u32> = HashMap::new();
         for line in group.lines() {
-            line
-                .chars()
-                .for_each(|c| {
-                    if let Some(val) = answers_map.insert(c, 1) {
-                        answers_map.insert(c, val + 1);
-                    }
-                });
+            line.chars().for_each(|c| {
+                if let Some(val) = answers_map.insert(c, 1) {
+                    answers_map.insert(c, val + 1);
+                }
+            });
         }
-        result += answers_map.iter().filter(|(_, val)| **val == mem_no).count() as u64
+        result += answers_map
+            .iter()
+            .filter(|(_, val)| **val == mem_no)
+            .count() as u64
     }
     result
 }
