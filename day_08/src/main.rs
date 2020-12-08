2eae639799c8d3b3e1029a69d581a51100b132bf
SilanSurfer <szwarc.adam@gmail.com>
2020-12-08 21:15:28 +0100

    feat: Solution for part 2 of day 8

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -7,8 +7,13 @@ fn main() {
     let contents = fs::read_to_string(file_path)
         .unwrap_or_else(|_| panic!("Couldn't open file {}", file_path));
     let input_vec = utils::get_lines_from_input(&contents);
-    println!(
-        "ACC value: {}",
-        solutions::get_acc_value(&input_vec)
-    );
+    println!("ACC value: {}", solutions::get_acc_value(&input_vec));
+    let file_path_fixed = "inputs/day_08_fixed.txt";
+    let contents = fs::read_to_string(file_path_fixed)
+        .unwrap_or_else(|_| panic!("Couldn't open file {}", file_path_fixed));
+    let input_vec_fixed = utils::get_lines_from_input(&contents);
+    // println!(
+    //     "ACC value with debugged input: {}",
+    //     solutions::get_acc_value_when_no_loop(&input_vec_fixed)
+    // );
 }
--- day_08/src/solutions.rs
+++ day_08/src/solutions.rs
@@ -1,4 +1,3 @@
-
 #[derive(Debug)]
 enum InstructionName {
     Acc,
@@ -28,17 +27,19 @@ impl Instruction {
             val,
             was_executed: false,
         }
-    }   
+    }
+    pub fn mark_executed(&mut self) {
+        self.was_executed = true;
+    }
 }
 
 pub fn get_acc_value(input: &[&str]) -> i32 {
-    let mut instructions: Vec<Instruction> = 
-        input.iter().map(|line| Instruction::new(line)).collect();
+    let mut instructions = map_input_to_instructions(input);
     let mut acc: i32 = 0;
     let mut index: i32 = 0;
     loop {
-        let mut instruction = &mut instructions[index as usize];
-        println!("Executing instruction {:?}", &instruction);
+        let instruction = &mut instructions[index as usize];
+        println!("[{}] Executing {:?}", index, &instruction);
         if !instruction.was_executed {
             match instruction.name {
                 InstructionName::Acc => {
@@ -49,18 +50,50 @@ pub fn get_acc_value(input: &[&str]) -> i32 {
                 InstructionName::Jmp => {
                     index += instruction.val;
                     println!("Jmp -> {}", index);
-
                 }
                 InstructionName::Nop => {
                     index += 1;
                     println!("Nop -> {}", index);
-
                 }
             }
-            instruction.was_executed = true;
+            instruction.mark_executed();
         } else {
             break;
         }
     }
     acc
+}
+
+pub fn get_acc_value_when_no_loop(input: &[&str]) -> i32 {
+    let mut instructions = map_input_to_instructions(input);
+    let mut acc: i32 = 0;
+    let mut index: i32 = 0;
+    while (index as usize) < instructions.len() {
+        let instruction = &mut instructions[index as usize];
+        println!("[{}] Executing {:?}", index, &instruction);
+        match instruction.name {
+            InstructionName::Acc => {
+                acc += instruction.val;
+                index += 1;
+                println!("Acc -> {}, next index -> {}", acc, index);
+            }
+            InstructionName::Jmp => {
+                index += instruction.val;
+                println!("Jmp -> {}", index);
+            }
+            InstructionName::Nop => {
+                index += 1;
+                println!("Nop -> {}", index);
+            }
+        }
+        instruction.mark_executed();
+    }
+    acc
+}
+
+fn map_input_to_instructions(input: &[&str]) -> Vec<Instruction> {
+    input
+        .iter()
+        .map(|line| Instruction::new(line))
+        .collect::<Vec<Instruction>>()
 }
