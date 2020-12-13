a65556bbb37d477d8caebfc011af5570474c78c2
SilanSurfer <szwarc.adam@gmail.com>
2020-12-13 22:35:59 +0100

    feat: Solution for part 2 day 12. Not working

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -3,12 +3,16 @@ use std::fs;
 mod solutions;
 
 fn main() {
-    let file_path = "inputs/day_08.txt";
+    let file_path = "inputs/day_12.txt";
     let contents = fs::read_to_string(file_path)
         .unwrap_or_else(|_| panic!("Couldn't open file {}", file_path));
     let input_vec = utils::get_lines_from_input(&contents);
     println!(
         "Manhattan distance is {}",
         solutions::get_manhattan_distance(&input_vec)
+    );
+    println!(
+        "Manhattan distance with waypoint is{}",
+        solutions::get_manhattan_distance_with_waypoint(&input_vec)
     );
 }
--- day_12/src/solutions.rs
+++ day_12/src/solutions.rs
@@ -28,6 +28,86 @@ struct Ferry {
     pos_y: i32,
 }
 
+#[derive(Debug)]
+struct Waypoint {
+    pos: (i32, i32),
+}
+
+#[derive(Debug)]
+struct FerryWithWaypoint {
+    ferry: (i32, i32),
+    waypoint: Waypoint,
+}
+
+impl FerryWithWaypoint {
+    fn new() -> Self {
+        FerryWithWaypoint {
+            ferry: (0, 0),
+            waypoint: Waypoint::new(),
+        }
+    }
+
+    fn apply(&mut self, action: &Action) {
+        match action.name {
+            'N' | 'S' | 'E' | 'W' => { self.waypoint.move_point(action); }
+            'L' => {
+                self.rotate_waypoint(action.value as i32);
+            }
+            'R' => {
+                self.rotate_waypoint(360 - action.value as i32);
+            }
+            'F' => {
+                self.ferry = (self.ferry.0 + self.waypoint.pos.0 * 10, self.ferry.1 + self.waypoint.pos.1 * 10);
+            }
+            _ => panic!("Unsupported action!!!"),
+        }
+    }
+
+    fn get_manhattan_distance(&self) -> u64 {
+        (self.ferry.0.abs() + self.ferry.1.abs())
+            .try_into()
+            .expect("Couldn't conver i32 to u64!")
+    }
+
+    fn rotate_waypoint(&mut self, angle: i32) {
+        let original: (f64, f64) = (self.waypoint.pos.0 as f64, self.waypoint.pos.1 as f64);
+        let angle_in_radians = (angle as f64).to_radians();
+        let rotated = (original.0 * angle_in_radians.cos() - original.1 * angle_in_radians.sin(),
+                       original.0 * angle_in_radians.sin() + original.1 * angle_in_radians.cos());
+        self.waypoint.pos = (rotated.0 as i32, rotated.1 as i32);
+    }
+}
+
+impl Waypoint {
+    fn new() -> Self {
+        Waypoint {
+            pos: (10, 1),
+        }
+    }
+
+    fn move_point(&mut self, action: &Action) {
+        match action.name {
+            'N' => {
+                self.pos.0 += action.value as i32;
+                println!("Action: {:#?} -> Waypoint: {:#?}", action, self);
+            }
+            'S' => {
+                self.pos.0 -= action.value as i32;
+                println!("Action: {:#?} -> Waypoint: {:#?}", action, self);
+            }
+            'E' => {
+                self.pos.1 += action.value as i32;
+                println!("Action: {:#?} -> Waypoint: {:#?}", action, self);
+            }
+            'W' => {
+                self.pos.1 -= action.value as i32;
+                println!("Action: {:#?} -> Waypoint: {:#?}", action, self);
+            }
+            _ => panic!("Waypoint doesn't support this operation")
+        }
+    }
+}
+
 impl Action {
     fn parse(line: &str) -> Action {
         let name = line
@@ -51,6 +131,7 @@ impl Direction {
     }
 }
 
+
 impl Ferry {
     fn new() -> Self {
         Ferry {
@@ -64,40 +145,47 @@ impl Ferry {
         match action.name {
             'N' => {
                 self.pos_y += action.value as i32;
+                println!("Action: {:#?} -> Ferry: {:#?}", action, self);
             }
             'S' => {
                 self.pos_y -= action.value as i32;
+                println!("Action: {:#?} -> Ferry: {:#?}", action, self);
             }
             'E' => {
                 self.pos_x += action.value as i32;
+                println!("Action: {:#?} -> Ferry: {:#?}", action, self);
             }
             'W' => {
                 self.pos_x -= action.value as i32;
+                println!("Action: {:#?} -> Ferry: {:#?}", action, self);
             }
             'L' => {
                 let rot_steps = action.value / 90;
                 self.direction = Direction::from_u32((self.direction as u32 + rot_steps) % 4);
+                println!("Action: {:#?} -> Ferry: {:#?}", action, self);
             }
             'R' => {
                 let rot_steps = action.value / 90;
-                self.direction = Direction::from_u32(
-                    ((self.direction as i32 - rot_steps as i32).abs() % 4)
-                        .try_into()
-                        .expect("Couldn't convert i32 to u32"),
-                );
+                self.direction =
+                    Direction::from_u32((4 + self.direction as u32 - rot_steps as u32) % 4);
+                println!("Action: {:#?} -> Ferry: {:#?}", action, self);
             }
             'F' => match self.direction {
                 Direction::East => {
                     self.pos_x += action.value as i32;
+                    println!("Action: {:#?} -> Ferry: {:#?}", action, self);
                 }
                 Direction::West => {
                     self.pos_x -= action.value as i32;
+                    println!("Action: {:#?} -> Ferry: {:#?}", action, self);
                 }
                 Direction::North => {
                     self.pos_y += action.value as i32;
+                    println!("Action: {:#?} -> Ferry: {:#?}", action, self);
                 }
                 Direction::South => {
                     self.pos_y -= action.value as i32;
+                    println!("Action: {:#?} -> Ferry: {:#?}", action, self);
                 }
             },
             _ => panic!("Unsupported action!!!"),
@@ -107,7 +195,7 @@ impl Ferry {
     fn get_manhattan_distance(&self) -> u64 {
         (self.pos_x.abs() + self.pos_y.abs())
             .try_into()
-            .expect("Couldn't conver i32 to u32!")
+            .expect("Couldn't conver i32 to u64!")
     }
 }
 
@@ -123,11 +211,30 @@ pub fn get_manhattan_distance(input: &[&str]) -> u64 {
     ferry.get_manhattan_distance()
 }
 
+pub fn get_manhattan_distance_with_waypoint(input: &[&str]) -> u64 {
+    let actions = input
+        .iter()
+        .map(|line| Action::parse(line))
+        .collect::<Vec<Action>>();
+    let mut ferry_with_waypoint = FerryWithWaypoint::new();
+    for action in actions {
+        ferry_with_waypoint.apply(&action);
+    }
+    println!("Ferry with waypoint: {:#?}", ferry_with_waypoint);
+    ferry_with_waypoint.get_manhattan_distance()
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn first_test() {
+    fn part_one_test() {
         let input = vec!["F10", "N3", "F7", "R90", "F11"];
         assert_eq!(25, super::get_manhattan_distance(&input));
+    }
+
+    #[test]
+    fn part_two_test() {
+        let input = vec!["F10", "N3", "F7", "R90", "F11"];
+        assert_eq!(286, super::get_manhattan_distance_with_waypoint(&input));
     }
 }
