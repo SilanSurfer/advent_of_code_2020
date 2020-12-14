4efdba63dc24ab28bc435e91ac1ebc6d2a9cafa7
SilanSurfer <szwarc.adam@gmail.com>
2020-12-14 22:39:54 +0100

    refactor: Modified solution for part 1 of day 6
    Removed state and everything is done in functional style.

--- day_06/src/solutions.rs
+++ day_06/src/solutions.rs
@@ -3,18 +3,16 @@ use std::collections::{HashMap, HashSet};
 /// For each group, count the number of questions to which anyone answered "yes".
 /// What is the sum of those counts?
 pub fn get_sum_of_questions_with_yes_answer(input: &[&str]) -> u64 {
-    let mut result: u64 = 0;
-    for group in input.iter() {
-        let mut set: HashSet<char> = HashSet::new();
-        for line in group.lines() {
-            line.chars().for_each(|c| {
-                set.insert(c);
-            });
-        }
-        // group.lines().map(|line| line.to_str()).chars().try_for_each(|c| set.insert(c));
-        result += set.iter().count() as u64;
-    }
-    result
+    input
+        .iter()
+        .map(|group| {
+            group
+                .lines()
+                .flat_map(|line| line.chars())
+                .collect::<HashSet<_>>()
+                .len()
+        })
+        .sum::<usize>() as u64
 }
 
 /// For each group, count the number of questions to which everyone answered "yes".
@@ -37,4 +35,13 @@ pub fn get_sum_of_questions_with_everyone_yes_answer(input: &[&str]) -> u64 {
             .count() as u64
     }
     result
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_first_part() {
+        let input: Vec<&str> = vec!["abc\n", "a\nb\nc\n", "ab\nac\n", "a\na\na\na\n", "b\n"];
+        assert_eq!(11, super::get_sum_of_questions_with_yes_answer(&input));
+    }
 }
