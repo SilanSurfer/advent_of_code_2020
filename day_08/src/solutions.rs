0cc25ac85d451ed3acf3888d120c5533d7d014db
SilanSurfer <szwarc.adam@gmail.com>
2020-12-22 22:04:43 +0100

    feat: Solution for part 2 of day 8
    Added unit tests and cleaned the code.

--- day_08/src/solutions.rs
+++ day_08/src/solutions.rs
@@ -1,11 +1,11 @@
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum InstructionName {
     Acc,
     Jmp,
     Nop,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Instruction {
     name: InstructionName,
     val: i32,
@@ -41,22 +41,9 @@ pub fn get_acc_value(input: &[&str]) -> i32 {
         let instruction = &mut instructions[index as usize];
         println!("[{}] Executing {:?}", index, &instruction);
         if !instruction.was_executed {
-            match instruction.name {
-                InstructionName::Acc => {
-                    acc += instruction.val;
-                    index += 1;
-                    println!("Acc -> {}, next index -> {}", acc, index);
-                }
-                InstructionName::Jmp => {
-                    index += instruction.val;
-                    println!("Jmp -> {}", index);
-                }
-                InstructionName::Nop => {
-                    index += 1;
-                    println!("Nop -> {}", index);
-                }
-            }
-            instruction.mark_executed();
+            let (inc_acc, inc_index) = exectute_instruction(instruction);
+            acc += inc_acc;
+            index += inc_index;
         } else {
             break;
         }
@@ -65,30 +52,57 @@ pub fn get_acc_value(input: &[&str]) -> i32 {
 }
 
 pub fn get_acc_value_when_no_loop(input: &[&str]) -> i32 {
-    let mut instructions = map_input_to_instructions(input);
-    let mut acc: i32 = 0;
-    let mut index: i32 = 0;
-    while (index as usize) < instructions.len() {
-        let instruction = &mut instructions[index as usize];
-        println!("[{}] Executing {:?}", index, &instruction);
-        match instruction.name {
-            InstructionName::Acc => {
-                acc += instruction.val;
-                index += 1;
-                println!("Acc -> {}, next index -> {}", acc, index);
-            }
+    let instructions = map_input_to_instructions(input);
+    for (no, elem) in instructions.iter().enumerate() {
+        let mut index: i32 = 0;
+        let mut acc: i32 = 0;
+        let mut instructions_modified = instructions.clone();
+        match elem.name {
+            InstructionName::Acc => continue,
             InstructionName::Jmp => {
-                index += instruction.val;
-                println!("Jmp -> {}", index);
+                instructions_modified[no].name = InstructionName::Nop;
+                println!("Modified jmp at {}", no);
             }
             InstructionName::Nop => {
-                index += 1;
-                println!("Nop -> {}", index);
+                instructions_modified[no].name = InstructionName::Jmp;
+                println!("Modified nop at {}", no);
             }
         }
-        instruction.mark_executed();
+        loop {
+            let instruction = &mut instructions_modified[index as usize];
+            if !instruction.was_executed {
+                let (inc_acc, inc_index) = exectute_instruction(instruction);
+                acc += inc_acc;
+                index += inc_index;
+            } else {
+                break;
+            }
+            if index as usize == instructions.len() {
+                println!("Finished last instruction, current index: {}", index);
+                return acc;
+            }
+        }
     }
-    acc
+    0
+}
+
+fn exectute_instruction(instruction: &mut Instruction) -> (i32, i32) {
+    let mut acc: i32 = 0;
+    let mut index: i32 = 0;
+    match instruction.name {
+        InstructionName::Acc => {
+            acc += instruction.val;
+            index += 1;
+        }
+        InstructionName::Jmp => {
+            index += instruction.val;
+        }
+        InstructionName::Nop => {
+            index += 1;
+        }
+    }
+    instruction.mark_executed();
+    (acc, index)
 }
 
 fn map_input_to_instructions(input: &[&str]) -> Vec<Instruction> {
@@ -96,4 +110,20 @@ fn map_input_to_instructions(input: &[&str]) -> Vec<Instruction> {
         .iter()
         .map(|line| Instruction::new(line))
         .collect::<Vec<Instruction>>()
+}
+
+#[cfg(test)]
+mod tests {
+    const INPUT: [&str; 9] = [
+        "nop +0", "acc +1", "jmp +4", "acc +3", "jmp -3", "acc -99", "acc +1", "jmp -4", "acc +6",
+    ];
+    #[test]
+    fn test_first_part() {
+        assert_eq!(5, super::get_acc_value(&INPUT));
+    }
+
+    #[test]
+    fn test_second_part() {
+        assert_eq!(8, super::get_acc_value_when_no_loop(&INPUT));
+    }
 }
